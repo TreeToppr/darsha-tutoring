5be4f0485c420a5aeca7236989e21379dd2de208
TreeToppr <darshawaterhouse@gmail.com>
2026-01-24 12:05:59 +1300

    4th redeploy fixing emails

--- src/app/api/email/booking-cancelled/route.js
+++ src/app/api/email/booking-cancelled/route.js
@@ -1,3 +1,5 @@
+export const runtime = "nodejs";
+
 import { NextResponse } from "next/server";
 // import { sendEmail } from "@/lib/email/resend";
 import { sendEmail } from "../../../../lib/email/resend";
